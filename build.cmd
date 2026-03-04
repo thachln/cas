acb8306b87df9107d9df260dadf1ee82337c46cd
Thach.Le <thachln@gmail.com>
2026-03-04 17:07:00 +0700

    Fix issues to build.

--- build.cmd
+++ build.cmd
@@ -1,2 +1,6 @@
 @ECHO OFF
+ECHO Install grgit-core-4.1.0.jar local maven from original grgit-core-4.1.1.jar
+cd work-around
+call prebuild.cmd
+cd ..
 gradlew build install --parallel -x test -x javadoc -x check --build-cache --configure-on-demand
---
+++ work-around/prebuild.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+mvn install:install-file -Dfile=grgit-core-4.1.0.jar -DgroupId=org.ajoberstar.grgit -DartifactId=grgit-core -Dversion=4.1.0 -Dpackaging=jar
